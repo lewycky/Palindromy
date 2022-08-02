49dc912198e5a5ddd3ab891bd8fbe5d4d9f53e66
Arek Lewicki <lewicki1arek@gmail.com>
2022-08-02 16:07:54 +0200

    Add function the_same

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("something")
+from ctypes.wintypes import WORD
+
+
+def the_same(word):
+    
+    check=''
+
+    for i in reversed(word):
+        check+=i
+       
+    if check==word:
+        return print("Wyraz jest Palindromem")
+    
+    elif check != word:   
+        
+        return print("Wyraz nie jest Palindromem")
+      
+
+the_same('potop')
